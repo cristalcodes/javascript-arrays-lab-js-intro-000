d2f3a49b3123b780dc1a37f0bc22d6a989b77290
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 18:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ function prependKitten(){
   return ['Arnold',...kittens]
 }
 
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1)
+}
+
 function removeFirstKitten(){
   return kittens.slice(1);
 }
