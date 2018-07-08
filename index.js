7e12524cb150d9b21556a27e807d5aac5f815400
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 16:14:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function arrays(){
-  kittens=['Milo','Otis','Garfield'];
+  var kittens=['Milo','Otis','Garfield'];
 }// Add your functions and code here
-
-function kittens(){
-  var kittens=['Milo','Otis','Garfield']
-}
