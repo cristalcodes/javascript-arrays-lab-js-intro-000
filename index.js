e388fc2cd2c0a3fc26e27921684328188bda4ed8
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 16:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function arrays(){
   var kittens=['Milo','Otis','Garfield'];
 }// Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kitten.unshift('Ralph')
+  return kitten
+}
