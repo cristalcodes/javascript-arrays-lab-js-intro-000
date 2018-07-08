fe0d250ec8303d7cf6aba6f6e20c574df09cca5a
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 16:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function arrays(){
   kittens=['Milo','Otis','Garfield'];
 }// Add your functions and code here
 
-function destructivelyAppendKitten('Ralph'){
-  kittens.shift('Ralph')
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
   return kittens
 }
