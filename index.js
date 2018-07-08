a495a1defa593da8a5bdc8e2baefce4172c9c1f8
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 16:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function arrays(){
-  var kittens=['Milo','Otis','Garfield'];
+  kittens=['Milo','Otis','Garfield'];
 }// Add your functions and code here
 
 function destructivelyAppendKitten(name){
